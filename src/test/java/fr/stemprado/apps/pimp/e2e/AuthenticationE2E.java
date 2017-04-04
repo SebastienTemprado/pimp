81a5f2dfa0d2be8fccf206695b84ffbaf2d38f70
Sébastien Temprado <stemprado@gmail.com>
2017-04-05 00:15:19 +0200

    e2e test : sign in

--- src/test/java/fr/stemprado/apps/pimp/e2e/AuthenticationE2E.java
+++ src/test/java/fr/stemprado/apps/pimp/e2e/AuthenticationE2E.java
@@ -3,21 +3,39 @@ package fr.stemprado.apps.pimp.e2e;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AuthenticationE2E extends AbstractE2ETest {
 	
 	@Test
 	public void hasToSignIn() {
-		 
-	     driver.get(baseUrl + "/pimp");
-//	     // Enter something to search for
-//	     element.sendKeys("Cheese!");
-	//
-//	     // Now submit the form. WebDriver will find the form for us from the element
-//	     element.submit();
-
-	     assertThat(driver.getTitle()).isEqualTo("Pimp - Login");
-//	     WebElement element = driver.findElement(By.name("q"));
+		// try to access a page without being authenticated
+		driver.get(baseUrl + "/pimp");
+		// forward to Login page
+	    assertThat(driver.getTitle()).isEqualTo("Pimp - Login");
+	    WebElement username = (new WebDriverWait(driver, 10))
+	    		  .until(ExpectedConditions.presenceOfElementLocated(By.id("username")));
+	    username.sendKeys("user");
+	    WebElement password = driver.findElement(By.id("password"));
+	    // wrong password 
+	    password.sendKeys("pass");
+	    WebElement submit = driver.findElement(By.id("signIn"));
+	    submit.click();
+	    // wrong password : stay on the login page
+	    assertThat(driver.getTitle()).isEqualTo("Pimp - Login");
+	    username = (new WebDriverWait(driver, 10))
+	    		  .until(ExpectedConditions.presenceOfElementLocated(By.id("username")));
+	    username.sendKeys("user");
+	    password = driver.findElement(By.id("password"));
+	    password.sendKeys("password");
+	    submit = driver.findElement(By.id("signIn"));
+	    submit.click();
+	    new WebDriverWait(driver, 10).until(ExpectedConditions.titleIs("Pimp"));
+	    // access granted
+	    assertThat(driver.getTitle()).isEqualTo("Pimp");
 	}
 
 }
