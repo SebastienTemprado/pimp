391bf60eb7b3bf4daa9c02678f66a762c2f4c229
Sébastien Temprado <stemprado@gmail.com>
2017-04-14 00:08:11 +0200

    F004: add UserDTO

---
+++ src/main/java/fr/stemprado/apps/pimp/beans/dtos/UserDTO.java
@@ -0,0 +1,70 @@
+package fr.stemprado.apps.pimp.beans.dtos;
+
+public class UserDTO {
+
+	/**
+	 * username : the login
+	 */
+	private String username;
+	
+	private String password;
+	
+	private String passwordConfirmation;
+	
+	private String lastname;
+	
+	private String firstname;
+	
+	private String email;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirmation() {
+		return passwordConfirmation;
+	}
+
+	public void setPasswordConfirmation(String passwordConfirmation) {
+		this.passwordConfirmation = passwordConfirmation;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
